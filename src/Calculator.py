8e3a3c9ac6553017f59c74dcdb8f851611f1063e
John Park <Rtos9er@gmail.com>
2025-05-20 05:57:25 +0900

    multiply

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,18 @@
 from functools import reduce
 
 
+def multiply(src):
+    if '*' in src:
+        tokens = src.split('*')
+        arr = map(lambda t: int(t), tokens)
+        return reduce(lambda acc, cur: acc * cur, arr, 1)
+    else:
+        return int(src)
+
+
 def plus(src):
     tokens = src.split('+')
-    arr = map(lambda t: int(t), tokens)
+    arr = map(lambda t: multiply(t), tokens)
     return reduce(lambda acc, cur: acc + cur, arr, 0)
 
 
@@ -12,6 +21,6 @@ def calculate(src):
     return val
 
 if __name__ == '__main__':
-    src = '1+2+3'
+    src = '1*2+3*2*2'
     val = calculate(src)
     print(val)
