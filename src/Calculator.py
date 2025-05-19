3ac982659bd393d8143ba11b0857ab6298421534
John Park <Rtos9er@gmail.com>
2025-05-20 07:23:58 +0900

    +-, -- 처리

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,7 +4,7 @@ from functools import reduce
 def divide(src):
     if '/' in src:
         tokens = src.split('/')
-        arr = list(map(int, tokens))
+        arr = list(map(float, tokens))
         token = arr[0]
         return reduce(lambda acc, cur: acc / cur, arr[1:], token)
     else:
@@ -40,14 +40,15 @@ def plus(src):
 
 
 def parenthesis(src):
+    mixSign = lambda s: s.replace('+-', '-').replace('--', '+')
     if '(' in src:
         start = src.index('(')
         val = parenthesis(src[start + 1:])
-        return parenthesis(src[0:start] + val)
+        return parenthesis(mixSign(src[0:start].strip() + val))
     elif ')' in src:
         end = src.index(')')
         val = str(plus(src[0:end]))
-        return val + src[end + 1:]
+        return mixSign(val + src[end + 1:].strip())
     else:
         return src
 
@@ -69,6 +70,22 @@ if __name__ == '__main__':
         ('1-2-3', -4),
         ('6 / 2 - 3', 0),
         ('\t5.3 ', 5.3),
+        ('5.3 - 0.3 ', 5.0),
+        ('1 + 2', 3),
+        ('1 + 2 + 3', 6),
+        ('2 * 3', 6),
+        ('1 + 2 * 3', 7),
+        ('2 * 3 + 3', 9),
+        ('1 - 3 - 3', -5),
+        ('1 - 3 + 5', 3),
+        ('1 + 3 - 3', 1),
+        ('8 / 2 - 1', 3),
+        ('9 / 3 - 2', 1),
+        ('(1 + 2) * 3', 9),
+        ('2 - (1 - 5)', 6),
+        ('( 100 - (100 - 200) ) * 30', 6000),
+        ('4 *(1 + ((2 * 5) /  (2 + 3))) * 3', 36),
+        ('((10 - (3 *3)) + 2) * ((10 / 5) +2)', 12),
     ]
 
 
