d479c51c4d9f5ce55d0cbdb2d046d38d850b2f8b
John Park <Rtos9er@gmail.com>
2025-05-20 05:21:27 +0900

    plus

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,8 +1,17 @@
-def calculate(src):
-    return src
+from functools import reduce
 
+
+def plus(src):
+    tokens = src.split('+')
+    arr = map(lambda t: int(t), tokens)
+    return reduce(lambda acc, cur: acc + cur, arr, 0)
+
+
+def calculate(src):
+    val = plus(src)
+    return val
 
 if __name__ == '__main__':
-    src = '1+2'
+    src = '1+2+3'
     val = calculate(src)
     print(val)
